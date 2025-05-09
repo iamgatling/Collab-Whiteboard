a9453dd783b6ddff5b2c42cac4e0cb015804370c
Gatling <95552807+iamgatling@users.noreply.github.com>
2025-05-09 18:30:10 +0100

    Update TextEditor.jsx to useRef

--- frontend /src/components/TextEditor.jsx
+++ frontend /src/components/TextEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function TextEditor({ socket, initialText = '', setLastUpdated }) {
   const [text, setText] = useState(initialText);
